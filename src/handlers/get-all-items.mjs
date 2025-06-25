5bb629ac3c7f91046c2679b9d9368e1579ebc603
EC2 Default User <ec2-user@ip-172-31-41-202.ec2.internal>
2025-06-25 10:14:24 +0000

    Cambio en get-all-items-mjs

--- src/handlers/get-all-items.mjs
+++ src/handlers/get-all-items.mjs
@@ -1,5 +1,7 @@
 // Create clients and set shared const values outside of the handler.
 
+// Esto es una modicación 
+
 // Create a DocumentClient that represents the query to add an item
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
